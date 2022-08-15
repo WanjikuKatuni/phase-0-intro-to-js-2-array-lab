5c5dcbae407b8db10d2012f3b7199e5eb76ae3a0
wanjiku <annritawanjiku77@gmail.com>
2022-08-15 12:48:09 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+
+// cats.length;
+// console.log(cats.length);
+// cats.push("Milo", "Otis", "Garfield");
+
+
+// cats.push("Ralph");
+// cats.unshift("Bob");
+
+function destructivelyAppendCat(){
+   
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(){
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+// function appendCat(name){
+
+//    newCats =[...cats, "Broom"];
+//    cats;
+//    newCats;
+// }
+// function appendCat(){
+
+//     const cats = ["Milo", "Otis", "Garfield"];
+//     console.log(cats);
+//     const appendCat = [...cats, "Broom"];
+
+//     // cats;
+//     // appendCat;
+
+//     console.log(appendCat);
+
+// }
+
+// function removeLastCat(){
+//     cats.slice(0,1);
+//     console.log(cats.slice(0,1));
+// }
+// function removeFirstCat(){
+//     cats.slice(1,2);
+// }
---
+++ test.js
@@ -0,0 +1,7 @@
+const cats = ["Milo", "Otis", "Garfield"];
+    console.log(cats);
+
+
+    newCats=[...cats,"Broom"];
+     
+     console.log(newCats);
